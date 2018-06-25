27b9bb1d966762e8ce5cef13eb69dd3cfb3356a6
niravajmeri <nirav.ajmeri@gmail.com>
2018-06-25 12:55:58 -0400

    Form updated + values correctly fetched

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,15 +8,19 @@ def main():
 @app.route('/explore',methods=['POST'])
 def explore():
     # read the posted values from the UI
-    _statement = request.form['inputStatement']
-    print(_statement)
+    statement = request.form['inputStatement']
+    #print(statement)
+    return render_template('explore.html', statement = statement)
+    #return redirect(url_for('main', statement = _statement))
  
     # validate the received values
-    if _statement:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
-  
+    # if _statement:
+        # return json.dumps({'html':'<span>All fields good !!</span>'})
+    # else:
+        # return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+		
+		
 if __name__ == "__main__":
   app.run() 
 
