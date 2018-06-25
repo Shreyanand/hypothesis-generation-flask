7e70b37569dddc15acbef7a5bfd5ae6205b7e0f2
niravajmeri <nirav.ajmeri@gmail.com>
2018-06-25 00:54:06 -0400

    included form + js

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-  return "Welcome!"
+  return render_template('index.html')
 
+@app.route('/explore',methods=['POST'])
+def explore():
+    # read the posted values from the UI
+    _statement = request.form['inputStatement']
+    print(_statement)
+ 
+    # validate the received values
+    if _statement:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+  
 if __name__ == "__main__":
   app.run() 
+
