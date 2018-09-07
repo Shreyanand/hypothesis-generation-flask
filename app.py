9056f754a980b8539def6933059b14bed65ba901
Shrey <shrey.anand@ymail.com>
2018-09-07 17:37:10 -0400

    Add sentiments and clean

--- app.py
+++ app.py
@@ -1,40 +1,60 @@
-from flask import Flask, render_template, json, request, redirect, url_for
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
-#Importing configuration
+from nltk.corpus import wordnet
+import pymysql
 import configparser
+from stanfordcorenlp import StanfordCoreNLP
+import gensim
+import json
+
 config = configparser.ConfigParser()
 config.read("config.txt")
-
 word2vec_path = config.get("configuration","word2vec_path")
 stanford_corenlp_path = config.get("configuration","stanford_corenlp_path")
+print(stanford_corenlp_path)
 
-import pymysql
+
 pymysql.install_as_MySQLdb()
-
 # Connect to DB
 mydbhost = config.get("configuration","mydbhost")
 mydbuser = config.get("configuration","mydbuser")
 mydbpasswd = config.get("configuration","mydbpasswd")
 mydbdb = config.get("configuration","mydbdb")
 
+
 # importing StandfordCoreNLP to tokenize, tag, and ner
 # Tree syntax of natural language: http://www.cs.cornell.edu/courses/cs474/2004fa/lec1.pdf
-from stanfordcorenlp import StanfordCoreNLP
 nlp = StanfordCoreNLP(stanford_corenlp_path)
+print('Core NLP instance')
 
 # Importing word2vec to find similarity and neighboring words
-import gensim
-from gensim.models import Word2Vec
-
 model = gensim.models.KeyedVectors.load_word2vec_format(word2vec_path, binary=True, limit=500000) 
+print('Word 2 Vec model ready')
 
-# Importing nltk and wordnet
-import nltk
-from nltk.corpus import wordnet
+def sentiments(word, nlp):
+    """ This function calls Stanford Core Nlp's sentiment annotator
+    
+        Args:
+            word (String): The input word for setiment detection
+            nlp (StanfordCoreNLP): Core nlp instance for sentiment detection
+        Returns:
+            String : Sentiment value: Very negative; Negative; Neutral; 
+            postive; Very positive
+    """
+    try:
+        sjson = nlp.annotate(word,properties={'annotators': 'sentiment','outputFormat': 'json','timeout': 1000})
+        res = json.loads(sjson)
+        sval = res["sentences"][0]["sentiment"]
+        return(sval)
+    except json.decoder.JSONDecodeError as e:
+        print (e)
+        
+    
+
 
 @app.route("/")
 def main():
+  print('Main Page opened')
   return render_template('index.html')
 
 @app.route('/explore',methods=['POST'])
@@ -44,12 +64,16 @@ def explore():
     # print("StanfordCoreNLP path:", stanford_corenlp_path)
 
     # read the posted values from the UI
+    print('explore')
     statement = request.form['inputStatement']
     print(statement)
-
-    sentence_tokens = nlp.word_tokenize(statement)
+    
+    
+    #sentence_tokens = nlp.word_tokenize(statement)
     sentence_tags = nlp.pos_tag(statement)
     sentence_ner = nlp.ner(statement)
+    statement_sentiment = "Sentiment of the query is: " + sentiments(statement, nlp)
+    print(statement_sentiment)
     # sentence_parse = nlp.parse(statement)
     # sentence_dependency = nlp.dependency_parse(statement)
 
@@ -61,22 +85,17 @@ def explore():
     to_replace_nouns = []
     to_replace_nounphrases = []
 
-    replacement_verbs_synonyms = []
-    replacement_verbs_antonyms = []
-    replacement_adjectives_synonyms = []
-    replacement_adjectives_antonyms = []
-    replacement_nouns_synonyms = []
-    replacement_nouns_antonyms = []
-
     for (i, j) in sentence_ner: 
-      if(j != 'O'): 
+      if(j != 'O'): #i = NCSU, j = ORGANISATION; i = sent, j = 0
         to_replace_ners.append((i, j))
 
     verb_check = 0
     noun_check = 0
     adj_check = 0
 
-    for (i, j) in sentence_tags: 
+    for (i, j) in sentence_tags: # Here verb checks are activated after a verb is discovered, so pair of verbs as verbphrases? Check Logic.
+        
+        
       if(verb_check == 1):
         verbphrase = verb + '_' + i
         to_replace_verbphrases.append((verbphrase, i))
@@ -106,10 +125,9 @@ def explore():
         to_replace_adjectives.append(i)
         adj = i
         adj_check = 1
-      
 
-    nlp.close()
 
+    ## These are using W2V, currently used.
     topk = 10
     replacement_ners = []
     replacement_verbs = []
@@ -119,61 +137,60 @@ def explore():
     replacement_adjectives = []
     replacement_adjphrases = []
 	
+    
+    ## Connect to Wikipedia Database of instance and class. 
+    ## For each ner, find similar ners using W2V and append to replacement_ners
+    ## For each similar ner, find it's category from simple types Wikipedia Database
+    
     connection = pymysql.connect(host=mydbhost, user=mydbuser, passwd=mydbpasswd, db=mydbdb)
     ner_categories = []
 	
     for (i, j) in to_replace_ners:
         try:
             similar_ners = model.most_similar([i, j.lower()], [], topk)
-            replacement_ners.append((i, similar_ners))
+            senti_ners = []
+            #nlp = StanfordCoreNLP(stanford_corenlp_path)
             for (similar_ner, score) in similar_ners:
+                print(similar_ner)
+                senti_ners.append([similar_ner, score, sentiments(similar_ner, nlp)])
                 connection = pymysql.connect(host=mydbhost, user=mydbuser, passwd=mydbpasswd, db=mydbdb)
                 try:
                   with connection.cursor() as cursor:
-			        # Execute SQL select statement
-                    query = "SELECT instance, class FROM simple_types where instance like (select longform from abbreviations where shortform = CONCAT('%', '"+ similar_ner + "' ,'%'))"
-                    cursor.execute(query)
-						
-                    # Get the number of rows in the resultset
-                    numrows = cursor.rowcount
-                    # Get and display one row at a time
-                    for x in range(0, numrows):
-                      row = cursor.fetchone()
-                      print(row[0], "-->", row[1])
-                      ner_categories.append((i, row[1]))
+#			        # Execute SQL select statement
+#                    query = "SELECT instance, class FROM simple_types where instance like CONCAT('%', '"+ similar_ner + "' ,'%')"
+#                    cursor.execute(query)
+#						
+#                    # Get the number of rows in the resultset
+#                    numrows = cursor.rowcount
+#                    # Get and display one row at a time
+#                    for x in range(0, numrows):
+#                      row = cursor.fetchone()
+#                      print(row[0], "-->", row[1])
+#                      ner_categories.append((i, row[1]))
+                      pass
                 # Close the connection
                 finally:
                   # Close connection.
                   connection.close()
-
+            replacement_ners.append((i, senti_ners))
         except KeyError as e:
             print(e)
     
-    #print(replacement_ners)
-        
+    print("Alternative NERs" + str(replacement_ners))
+    
+    
+    ## Use W2V for replacing nouns, verbs, adj verbphrases, nounphrases, and adj phrases
+    
     for verb in to_replace_verbs:
         try: 
             similar_verbs = model.most_similar(verb, [], topk)
-            replacement_verbs.append((verb,similar_verbs))
+            senti_verbs = [[i, j, sentiments(i, nlp)] for (i, j) in similar_verbs]
+            replacement_verbs.append((verb, senti_verbs))
         except KeyError as e:
             print(e)
- 
-    #print(replacement_verbs)
-
-    for verb in to_replace_verbs:
-        for syn in wordnet.synsets(verb): 
-            for l in syn.lemmas():
-                replacement_verbs_synonyms.append(l.name())
-                if l.antonyms():
-                    #print('L.Antonyms:', l.antonyms())
-				    #replacement_verbs_antonyms.append(l.antonyms()[0].name())
-                    for m in l.antonyms():
-                        replacement_verbs_antonyms.append(m.name())
-
-    print('Replacement Verb Synonyms', set(replacement_verbs_synonyms))
-    print('Replacement Verb Antonyms', set(replacement_verbs_antonyms))
-
-    
+            
+    print("Alternative Verbs" +  str(replacement_verbs))
+            
     for (verbphrase, nn) in to_replace_verbphrases:
         try:
             similar_verbphrases = model.most_similar([verbphrase, nn], [], topk)
@@ -181,30 +198,18 @@ def explore():
         except KeyError as e:
             print(e)
     
-    #print(replacement_verbphrases)
-    
+    print("Alternative Verb Phrases" + str(replacement_verbphrases))
+ 
     for noun in to_replace_nouns:
         try:
             similar_nouns = model.most_similar(noun, [], topk)
-            replacement_nouns.append((noun, similar_nouns))
+            senti_nouns = [[i, j, sentiments(i, nlp)] for (i, j) in similar_nouns]
+            replacement_nouns.append((noun, senti_nouns))
         except KeyError as e:
             print(e)
     
-    #print(replacement_nouns)
-	
-    for noun in to_replace_nouns:
-        for syn in wordnet.synsets(noun): 
-            for l in syn.lemmas():
-                replacement_nouns_synonyms.append(l.name())
-                if l.antonyms():
-                    #print('L.Antonyms:', l.antonyms())
-                    #replacement_nouns_antonyms.append(l.antonyms()[0].name())
-                    for m in l.antonyms():
-                        replacement_nouns_antonyms.append(m.name())
-					
-    print('Replacement Noun Synonyms', set(replacement_nouns_synonyms))
-    print('Replacement Noun Antonyms', set(replacement_nouns_antonyms))		
-
+    print("Alternative Noun" + str(replacement_nouns))
+    
     for (nounphrase, nn) in to_replace_nounphrases:
         try:
             similar_nounphrases = model.most_similar([nounphrase, nn], [], topk)
@@ -212,30 +217,18 @@ def explore():
         except KeyError as e:
             print(e)
     
-    #print(replacement_nounphrases)
+    print("Alternative Noun Phrases" + str(replacement_nounphrases))
     
     for adjective in to_replace_adjectives: 
         try: 
             similar_adjectives = model.most_similar(adjective, [], topk)
-            replacement_adjectives.append((adjective, similar_adjectives))
+            senti_adjectives = [[i, j, sentiments(i, nlp)] for (i, j) in similar_adjectives]
+            replacement_adjectives.append((adjective, senti_adjectives))
         except KeyError as e:
             print(e)
         
-    #print(replacement_adjectives)
-	
-    for adjective in to_replace_adjectives:
-        for syn in wordnet.synsets(adjective): 
-            for l in syn.lemmas():
-                replacement_adjectives_synonyms.append(l.name())
-                if l.antonyms():
-                    #print('L.Antonyms:', l.antonyms())
-                    #replacement_adjectives_antonyms.append(l.antonyms()[0].name())
-                    for m in l.antonyms():
-                        replacement_adjectives_antonyms.append(m.name())
-					
-    print('Replacement Adj Synonyms', set(replacement_adjectives_synonyms))
-    print('Replacement Adj Antonyms', set(replacement_adjectives_antonyms))	
-
+    print("Alternative Adjectives" + str(replacement_adjectives))
+    
     for (adjphrase, nn) in to_replace_adjphrases:
         try:
             similar_adjphrases = model.most_similar([adjphrase, nn], [], topk)
@@ -243,10 +236,70 @@ def explore():
         except KeyError as e:
             print(e)
     
-    #print(replacement_adjphrases)
-
+    print("Alternative Adjectives" + str(replacement_adjphrases))
     
-    return render_template('explore.html', statement = statement, replacement_ners = replacement_ners, replacement_verbs = replacement_verbs, replacement_verbphrases = replacement_verbphrases, replacement_nouns = replacement_nouns, replacement_nounphrases = replacement_nounphrases, replacement_adjectives = replacement_adjectives, replacement_adjphrases = replacement_adjphrases, ner_categories = ner_categories)
+## Experiment with wordnet for replacing verbs, nouns, and adjectives
+## Not yet used.
+#    replacement_verbs_synonyms = []
+#    replacement_verbs_antonyms = []
+#    replacement_adjectives_synonyms = []
+#    replacement_adjectives_antonyms = []
+#    replacement_nouns_synonyms = []
+#    replacement_nouns_antonyms = [] 
+#    for verb in to_replace_verbs:
+#        for syn in wordnet.synsets(verb): 
+#            for l in syn.lemmas():
+#                replacement_verbs_synonyms.append(l.name())
+#                if l.antonyms():
+#                    #print('L.Antonyms:', l.antonyms())
+#				    #replacement_verbs_antonyms.append(l.antonyms()[0].name())
+#                    for m in l.antonyms():
+#                        replacement_verbs_antonyms.append(m.name())
+#
+#    print('Replacement Verb Synonyms', set(replacement_verbs_synonyms))
+#    print('Replacement Verb Antonyms', set(replacement_verbs_antonyms))
+#
+#	
+#    for noun in to_replace_nouns:
+#        for syn in wordnet.synsets(noun): 
+#            for l in syn.lemmas():
+#                replacement_nouns_synonyms.append(l.name())
+#                if l.antonyms():
+#                    #print('L.Antonyms:', l.antonyms())
+#                    #replacement_nouns_antonyms.append(l.antonyms()[0].name())
+#                    for m in l.antonyms():
+#                        replacement_nouns_antonyms.append(m.name())
+#					
+#    print('Replacement Noun Synonyms', set(replacement_nouns_synonyms))
+#    print('Replacement Noun Antonyms', set(replacement_nouns_antonyms))		
+#
+#	
+#    for adjective in to_replace_adjectives:
+#        for syn in wordnet.synsets(adjective): 
+#            for l in syn.lemmas():
+#                replacement_adjectives_synonyms.append(l.name())
+#                if l.antonyms():
+#                    #print('L.Antonyms:', l.antonyms())
+#                    #replacement_adjectives_antonyms.append(l.antonyms()[0].name())
+#                    for m in l.antonyms():
+#                        replacement_adjectives_antonyms.append(m.name())
+#					
+#    print('Replacement Adj Synonyms', set(replacement_adjectives_synonyms))
+#    print('Replacement Adj Antonyms', set(replacement_adjectives_antonyms))	
+
+    nlp.close()
+    
+    return render_template ('explore.html', 
+                           statement = statement,
+                           statement_sentiment = statement_sentiment,
+                           replacement_ners = replacement_ners,
+                           replacement_verbs = replacement_verbs,
+                           replacement_verbphrases = replacement_verbphrases,
+                           replacement_nouns = replacement_nouns,
+                           replacement_nounphrases = replacement_nounphrases,
+                           replacement_adjectives = replacement_adjectives,
+                           replacement_adjphrases = replacement_adjphrases,
+                           ner_categories = ner_categories)
     #return redirect(url_for('main', statement = _statement))
  
     # validate the received values
